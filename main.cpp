b97f0fa17c27279aeb5b719105b3a88c8e05d6c7
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-06 12:22:43 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,41 @@
 #include <cassert>
 #include "TelCoColorCoder.h"
 
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor) {
+    TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(TelCoColorCoder::MajorColor major, TelCoColorCoder::MinorColor minor, int expectedPairNumber) {
+    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
+
 int main() {
     // Testing number to color pair
-    TelCoColorCoder::testNumberToPair(4, TelCoColorCoder::BLACK);
-    TelCoColorCoder::testNumberToPair(5, TelCoColorCoder::BLUE);
+    testNumberToPair(4, TelCoColorCoder::BLACK, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
     // Testing color pair to number
-    TelCoColorCoder::testPairToNumber(TelCoColorCoder::ORANGE, 7);
-    TelCoColorCoder::testPairToNumber(TelCoColorCoder::SLATE, 10);
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     // Print the color coding reference table
     TelCoColorCoder::printColorCodingReference();
 
     // Print color descriptions
     TelCoColorCoder::printColorDescriptions();
+
+    // Print all color pairs
+    TelCoColorCoder::printAllColorPairs();
+
+    // Check if a color pair is valid
+    std::cout << "Is (RED, BLUE) a valid color pair? " 
+              << (TelCoColorCoder::isValidColorPair(TelCoColorCoder::RED, TelCoColorCoder::BLUE) ? "Yes" : "No") 
+              << std::endl;
 
     return 0;
 }
