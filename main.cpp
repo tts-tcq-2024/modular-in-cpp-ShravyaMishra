a1a4e883fd635b41b116e74ed8121c7e3784c0f6
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-08-06 12:57:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <cassert>
-#include "COLORCODER.h"
+#include "COLORCODE.h"
 
 void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajor, TelCoColorCoder::MinorColor expectedMinor) {
     TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
